cc084214a0194d5c4bae73ec02c10b9849423a2c
Subhajit Maji <subhajitmaji2607@gmail.com>
2021-12-05 01:04:21 +0530

    create json-server and  routing

---
+++ src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+<h1>Hello you are on Home Compponent</h1>
+</template>
+
+<script>
+export default {
+    name:'Home'
+}
+</script>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,32 +1,55 @@
 <template>
-    <img class='logo' alt="Vue logo" src="../assets/logo.png">
-    <div class='box'>
-        <h2>Sign Up For Free</h2>
-        <input class='input' type='text' placeholder='Enter Name'/>
-        <input class='input' type='text' placeholder='Enter email'/>
-        <button class='btn'>Sign up</button>
-    </div>
+<img class='logo' alt="Vue logo" src="../assets/logo.png">
+<div class='box'>
+    <h2>Sign Up For Free</h2>
+    <input v-model='name' class='input' type='text' placeholder='Enter Name' />
+    <input v-model='email' class='input' type='text' placeholder='Enter email' />
+    <button v-on:click='onClickSignup' class='btn'>Sign up</button>
+</div>
 </template>
 
 <script>
-
+import axios from 'axios';
 export default {
-    name: 'Signup'
+    name: 'Signup',
+    data() { //data handel the data of the component
+        return {
+            name: '',
+            email: ''
+        }
+    },
+    methods: { //methods handle any type of function in that component
+        async onClickSignup() {
+            const newUser = await axios.post('http://localhost:3000/users', {
+                name: this.name,
+                email: this.email
+            })
+            this.name = ''
+            this.email = ''
+            localStorage.setItem('logged-in-user-id', newUser.data.id)
+            if(newUser.status === 201){
+                this.$router.push({name:'Home'})
+            }
+        }
+    }
+
 }
 </script>
 
 <style>
-.logo{
+.logo {
     width: 100px
 }
-.box{
-    /*border: 2px solid black;*/   
+
+.box {
+    /*border: 2px solid black;*/
     height: 400px;
     width: 700px;
     margin-left: auto;
     margin-right: auto
 }
-.input{
+
+.input {
     display: block;
     margin-left: auto;
     margin-right: auto;
@@ -37,7 +60,8 @@ export default {
     border: 2px solid skyblue;
     font-size: 20px;
 }
-.btn{
+
+.btn {
     width: 74%;
     height: 50px;
     color: #edf4f7;
