4aab9a0ccafd4dbcf57c20af8144ebfb58dca767
Subhajit Maji <subhajitmaji2607@gmail.com>
2021-12-05 21:56:22 +0530

    alamost done

---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+<div class='navbar'>
+    <router-link to='/'>Home</router-link>
+    <router-link to='/add'>Add Restaurant</router-link>
+    <router-link to='/update'>update Restaurant</router-link>
+    <router-link to='/login'>Login</router-link>
+    <router-link to='/signup'>Signup</router-link>
+    <router-link v-on:click= 'onClickLogout' to='#'>Log out</router-link>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    methods:{
+        onClickLogout(){
+            localStorage.clear()
+            this.$router.push({name:'Login'})
+        }
+    },
+
+}
+</script>
+
+<style scoped>
+.navbar {
+    /*border: 2px solid black;*/
+    min-height: 60px;
+    margin-bottom: 20px;
+    display: flex;
+    background: white;
+    justify-content: flex-end;
+    align-items: center;
+}
+
+.navbar a {
+    /*border: 2px solid black;*/
+    text-decoration: none;
+    width: 10%;
+    font-size: 20px;
+    color: black
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,53 @@
 <template>
-<h1>Hello you are on Home Compponent</h1>
+<h1>Hello {{ name }} you are on Home Compponent</h1>
+<table border='1' class='table'>
+    <tr>
+        <td>Serial No</td>
+        <td>Name</td>
+        <td>Contract</td>
+    </tr>
+    <tr v-for='item in retaurdants' :key='item.id'>
+        <td>{{item.id}}</td>
+        <td>{{item.name}}</td>
+        <td>{{item.contract}}</td>
+    </tr>
+</table>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-    name:'Home',
-    mounted(){      //mounted is a life cycle method which runs everytime when the page is render
-    // console.log('home',localStorage.getItem('logged-in-user-id'))
-        if(localStorage.getItem('logged-in-user-id')=== null) this.$router.push({name:'Signup'})
-        // !(localStorage.getItem('logged-in-user-id'))
+    name: 'Home',
+    data() {
+        return {
+            name: '',
+            retaurdants: [],
+        }
+    },
+    async mounted() { //mounted is a life cycle method which runs everytime when the page is render
+        // console.log('home',localStorage.getItem('logged-in-user-id'))
+        const userSuccessfullySignup = localStorage.getItem('logged-in-user-id')
+        const userSuccessfullyLogin = localStorage.getItem('userLogin')
+        if(userSuccessfullySignup === null || userSuccessfullyLogin === null) this.$router.push({name:'Login'})
+        // if (localStorage.getItem('logged-in-user-id') === null && !localStorage.getItem('userLogin')) this.$router.push({
+        //     name: 'Signup'
+        // })
+        this.name = JSON.parse(localStorage.getItem('userInfo'))?.name
+        console.log(userSuccessfullyLogin,userSuccessfullySignup)
         // ? this.$router.push({name:'Signup'}) : this.$router.push({name:'Login'})
-        // const userNotLoggedIn = !(localStorage.getItem('userLogin'));
-        // if(userNotLoggedIn) this.$router.push({name:'Login'})
+        const result = await axios.get('http://localhost:3000/retaurdant')
+        this.retaurdants = result.data
     }
 }
 </script>
+
+<style>
+td {
+    width: 160px;
+    height: 40px;
+}
+.table{
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
